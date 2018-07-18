53aeaa7f23577bdfffa4ce1c131964b742b5ddd8
fredski33 <thinkb4sink@gmail.com>
2018-07-18 23:53:49 +0200

    wip FOSUBQuest

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,14 +3,15 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
 
 /**
  * User
  *
- * @ORM\Table(name="user")
+ * @ORM\Table(name="`user`")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
  */
-class User
+class User extends BaseUser
 {
 
     /**
@@ -35,7 +36,7 @@ class User
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
 
     /**
      * @var string
